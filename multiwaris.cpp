b845ec430eeefd237ced6e4bfd0575ea91e54b95
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2025-06-03 11:17:28 +0700

    membuat class dasar orang dengan atribut umur

---
+++ multiwaris.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class pekerja : virtual public orang {
+public:
+
+    pekerja(int pUmur):
+        orang(pUmur)
+    {
+        cout << "Pekerja dibuat\n" << endl;
+    }
+};
+
